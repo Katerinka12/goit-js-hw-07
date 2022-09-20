e59a9f579fe5eb96ec7542f8442a51af634d1957
Katerinka12 <103268877+Katerinka12@users.noreply.github.com>
2022-09-20 11:31:52 +0300

    Delete js directory

--- js/task-07.js
+++
@@ -1,5 +0,0 @@
-const inputText = document.querySelector("#font-size-control");
-const spanText = document.querySelector('#text');
-
-spanText.style.fontSize = `${inputText.value}px`;
-inputText.addEventListener('input', e => {spanText.style.fontSize = `${inputText.value}px`})
